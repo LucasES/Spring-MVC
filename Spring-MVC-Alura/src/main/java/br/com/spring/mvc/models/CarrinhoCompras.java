851aecd886ffe5451b5ab4cbd5bc1ee2ec4969a6
Lucas Araújo <lucas.compufc@gmail.com>
2017-09-20 20:46:51 -0300

    Mudando o escopo das classes referente ao carrinho de compras.

--- Spring-MVC-Alura/src/main/java/br/com/spring/mvc/models/CarrinhoCompras.java
+++ Spring-MVC-Alura/src/main/java/br/com/spring/mvc/models/CarrinhoCompras.java
@@ -3,9 +3,12 @@ package br.com.spring.mvc.models;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION)
 public class CarrinhoCompras {
 
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<CarrinhoItem, Integer>();
@@ -23,6 +26,7 @@ public class CarrinhoCompras {
 	}
 	
 	public int getQuantidade() {
-		return itens.values().stream().reduce(0, (proximo, acumulador) -> proximo + acumulador);
+		return itens.values().stream()
+				.reduce(0, (proximo, acumulador) -> proximo + acumulador);
 	}
 }
