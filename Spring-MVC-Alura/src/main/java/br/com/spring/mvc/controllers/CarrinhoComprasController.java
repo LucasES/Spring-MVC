112eba9c997bee4f9f38ea932de1a9e3d4f695b5
Lucas Araújo <lucas.compufc@gmail.com>
2017-09-20 22:51:00 -0300

    Adicionando funcionalidade de carrinho de compras

--- Spring-MVC-Alura/src/main/java/br/com/spring/mvc/controllers/CarrinhoComprasController.java
+++ Spring-MVC-Alura/src/main/java/br/com/spring/mvc/controllers/CarrinhoComprasController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,18 +26,23 @@ public class CarrinhoComprasController {
 	private CarrinhoCompras carrinho;
 	
 	@RequestMapping("/add")
-	public ModelAndView add(Long produtoId, TipoPreco tipo){
-	    ModelAndView modelAndView = new ModelAndView("redirect:/produtos");
-	    CarrinhoItem carrinhoItem = criaItem(produtoId, tipo);
+	public ModelAndView add(Long produtoId, TipoPreco tipoPreco){
+	    ModelAndView modelAndView = new ModelAndView("redirect:/carrinho");
+	    CarrinhoItem carrinhoItem = criaItem(produtoId, tipoPreco);
 	    
 	    carrinho.add(carrinhoItem);
 	    
 	    return modelAndView;
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView itens(){
+	    return new ModelAndView("/carrinho/itens");
+	}
 
-	private CarrinhoItem criaItem(Long produtoId, TipoPreco tipo){
+	private CarrinhoItem criaItem(Long produtoId, TipoPreco tipoPreco){
 	    Produto produto = produtoDao.find(produtoId);
-	    CarrinhoItem carrinhoItem = new CarrinhoItem(produto, tipo);
+	    CarrinhoItem carrinhoItem = new CarrinhoItem(produto, tipoPreco);
 	    return carrinhoItem;
 	}
 }
