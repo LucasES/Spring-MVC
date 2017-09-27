b3c6ef3a37d3b10fea8ef34fa4bbf6309b5026b1
Lucas Araújo <lucas.compufc@gmail.com>
2017-09-26 22:51:10 -0300

    Configurando Guava para gerenciar o cache da aplicação

--- Spring-MVC-Alura/src/main/java/br/com/spring/mvc/config/AppWebConfiguration.java
+++ Spring-MVC-Alura/src/main/java/br/com/spring/mvc/config/AppWebConfiguration.java
@@ -1,5 +1,11 @@
 package br.com.spring.mvc.config;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,59 +22,71 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.google.common.cache.CacheBuilder;
+
 import br.com.spring.mvc.controllers.HomeController;
 import br.com.spring.mvc.dao.ProdutoDAO;
 import br.com.spring.mvc.infra.FileSaver;
 import br.com.spring.mvc.models.CarrinhoCompras;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses={HomeController.class,
-		ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class})
-public class AppWebConfiguration extends WebMvcConfigurerAdapter{
+@ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
+@EnableCaching
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
-		
+
 		resolver.setExposedContextBeanNames("carrinhoCompras");
-		
+
 		return resolver;
 	}
-	
-	@Bean
-	public MessageSource messageSource(){
-	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-	    messageSource.setBasename("/WEB-INF/messages");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    messageSource.setCacheSeconds(1);
-	    return messageSource;
-	}
-	
-	@Bean
-	public FormattingConversionService mvcConversionService(){
-	    DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
-	    DateFormatterRegistrar formatterRegistrar = new DateFormatterRegistrar();
-	    formatterRegistrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
-	    formatterRegistrar.registerFormatters(conversionService);
 
-	    return conversionService;
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/WEB-INF/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(1);
+		return messageSource;
 	}
-	
+
+	@Bean
+	public FormattingConversionService mvcConversionService() {
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		DateFormatterRegistrar formatterRegistrar = new DateFormatterRegistrar();
+		formatterRegistrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
+		formatterRegistrar.registerFormatters(conversionService);
+
+		return conversionService;
+	}
+
 	@Bean
 	public MultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
 	}
-	
+
 	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
-	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-	
+
+	@Bean
+	public CacheManager cacheManager() {
+		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(5,
+				TimeUnit.MINUTES);
+
+		GuavaCacheManager manager = new GuavaCacheManager();
+		manager.setCacheBuilder(builder);
+
+		return manager;
+	}
 }
