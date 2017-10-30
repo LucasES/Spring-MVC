351b77a41cbb94e2591ed3b51fe63abbbd14ca15
Lucas Araújo <lucas.compufc@gmail.com>
2017-10-30 17:50:04 -0200

    Configurando login e logout

--- Spring-MVC-Alura/src/main/java/br/com/spring/mvc/config/SecurityConfiguration.java
+++ Spring-MVC-Alura/src/main/java/br/com/spring/mvc/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.spring.mvc.dao.UsuarioDAO;
 
@@ -21,12 +22,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	    http.authorizeRequests()
 	    .antMatchers("/produtos/form").hasRole("ADMIN")
 	    .antMatchers("/carrinho/**").permitAll()
-	    .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
 	    .antMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
+	    .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
 	    .antMatchers("/produtos/**").permitAll()
+	    .antMatchers("/resources/**").permitAll()
 	    .antMatchers("/").permitAll()
 	    .anyRequest().authenticated()
-	    .and().formLogin();
+	    .and().formLogin().loginPage("/login").permitAll()
+	    .and().logout()
+	    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 	
 	@Override
